fd0cca40f13e8b7fb71e434fa7558c73606398df
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2024-08-29 10:24:52 +0100

    send email with title

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = "d4e5768f3c82495d8a3601f9a0dec894"
 url = ("https://newsapi.org/v2/everything?q=tesla&"
@@ -11,7 +12,11 @@ request = requests.get(url)
 # Get a dictionary with data
 content = request.json()
 
+body = ""
 # Access the article titles and description
 for article in content['articles']:
-    print(article['title'])
-    print(article['description'])
+    if article['title'] is not None:
+        body = body + article["title"] + "\n" +  article["description"] + 2 * "\n"
+
+body=body.encode("utf-8")
+send_email(message=body)
